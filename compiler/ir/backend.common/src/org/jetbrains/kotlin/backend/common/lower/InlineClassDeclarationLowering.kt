7011d83f3847e63e6f478cfbe36105ae565a5068
Svyatoslav Kuzmich <svyatoslav.kuzmich@jetbrains.com>
2018-11-13 15:02:56 +0300

    [JS IR BE] Fix parameters parents

--- compiler/ir/backend.common/src/org/jetbrains/kotlin/backend/common/lower/InlineClassDeclarationLowering.kt
+++ compiler/ir/backend.common/src/org/jetbrains/kotlin/backend/common/lower/InlineClassDeclarationLowering.kt
@@ -10,6 +10,7 @@ import org.jetbrains.kotlin.backend.common.ClassLoweringPass
 import org.jetbrains.kotlin.backend.common.FileLoweringPass
 import org.jetbrains.kotlin.backend.common.descriptors.WrappedSimpleFunctionDescriptor
 import org.jetbrains.kotlin.backend.common.ir.copyTo
+import org.jetbrains.kotlin.backend.common.ir.copyTypeParametersFrom
 import org.jetbrains.kotlin.descriptors.Modality
 import org.jetbrains.kotlin.ir.IrStatement
 import org.jetbrains.kotlin.ir.builders.*
@@ -246,7 +247,7 @@ class InlineClassLowering(val context: BackendContext) {
                 is IrConstructor -> function.parentAsClass.defaultType
                 else -> error("Unknown function type")
             }
-            typeParameters += function.typeParameters
+            copyTypeParametersFrom(function)
             annotations += function.annotations
             dispatchReceiverParameter = null
             extensionReceiverParameter = function.extensionReceiverParameter?.copyTo(this)
