6355172e8460cf57539b6cddf0eb8739298f74ad
Ilmir Usmanov <ilmir.usmanov@jetbrains.com>
2019-03-20 15:45:37 +0300

    Do not generate spilled coroutine fields multiple times
    Otherwise, there will be VerifyError at runtime.
    Previously, when suspend lambdas did not have invokeSuspend and
    invokeSuspend$$forInline, there could be only two kinds of suspend
    lambdas:
    1) with state-machine and spilled variables
    2) without both of them
    
    Currently, however, we do not have the second kind. So, on
    transformation of the lambda, we generate another field with the same
    name.
    
    This fix avoids it by memorizing FieldVisitors of transformed lambda,
    so, when CoroutineTransformerMethodVisitor generates 'new' fields, it
    reuses the old ones.

--- compiler/backend/src/org/jetbrains/kotlin/codegen/inline/RemappingClassBuilder.java
+++ compiler/backend/src/org/jetbrains/kotlin/codegen/inline/RemappingClassBuilder.java
@@ -26,12 +26,16 @@ import org.jetbrains.org.objectweb.asm.AnnotationVisitor;
 import org.jetbrains.org.objectweb.asm.ClassVisitor;
 import org.jetbrains.org.objectweb.asm.FieldVisitor;
 import org.jetbrains.org.objectweb.asm.MethodVisitor;
-import org.jetbrains.org.objectweb.asm.commons.*;
 import org.jetbrains.org.objectweb.asm.commons.FieldRemapper;
+import org.jetbrains.org.objectweb.asm.commons.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class RemappingClassBuilder extends DelegatingClassBuilder {
     private final ClassBuilder builder;
     private final Remapper remapper;
+    private final Map<String, FieldVisitor> spilledCoroutineVariables = new HashMap<>();
 
     public RemappingClassBuilder(@NotNull ClassBuilder builder, @NotNull Remapper remapper) {
         this.builder = builder;
@@ -68,9 +72,44 @@ public class RemappingClassBuilder extends DelegatingClassBuilder {
             @Nullable String signature,
             @Nullable Object value
     ) {
-        return new FieldRemapper(
-                builder.newField(origin, access, name, remapper.mapDesc(desc), remapper.mapSignature(signature, true), value), remapper
+        if (spilledCoroutineVariables.containsKey(name)) return spilledCoroutineVariables.get(name);
+        FieldRemapper field = new FieldRemapper(
+                builder.newField(origin, access, name, remapper.mapDesc(desc), remapper.mapSignature(signature, true), value),
+                remapper
         );
+        if (isSpilledCoroutineVariableName(name)) {
+            spilledCoroutineVariables.put(name, field);
+        }
+        return field;
+    }
+
+    /*
+     * Spilled coroutine variable are named in the following format:
+     * 1) First letter of its type
+     * 2) $
+     * 3) A number to differentiate them
+     */
+    private static boolean isSpilledCoroutineVariableName(String name) {
+        if (name.length() < 3) return false;
+        switch (name.charAt(0)) {
+            case 'L':
+            case 'Z':
+            case 'C':
+            case 'B':
+            case 'S':
+            case 'I':
+            case 'F':
+            case 'J':
+            case 'D':
+                break;
+            default:
+                return false;
+        }
+        if (name.charAt(1) != '$') return false;
+        for (int i = 2; i < name.length(); ++i) {
+            if (!Character.isDigit(name.charAt(i))) return false;
+        }
+        return true;
     }
 
     @Override
