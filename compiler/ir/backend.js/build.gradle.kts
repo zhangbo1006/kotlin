41f649d42da01cb702c9d34be586befff27d3bfc
Zalim Bashorov <zalim.bashorov@jetbrains.com>
2019-03-27 23:08:59 +0300

    ~ add experimental flags to affected builds

--- compiler/ir/backend.js/build.gradle.kts
+++ compiler/ir/backend.js/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
     kotlin("jvm")
     id("jps-compatible")
@@ -21,3 +23,10 @@ sourceSets {
     "test" {}
 }
 
+val compileKotlin: KotlinCompile by tasks
+
+compileKotlin.kotlinOptions {
+    freeCompilerArgs = freeCompilerArgs + listOf(
+        "-Xuse-experimental=kotlin.Experimental",
+        "-Xuse-experimental=org.jetbrains.kotlin.ir.DescriptorInIrDeclaration")
+}
